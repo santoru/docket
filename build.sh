2d83e3c9e93569ea5fd2a909e686f4cd0dc0e25e
santoru <alessiosantoru@gmail.com>
2026-05-21 23:22:14 +0200

    v1.1.0 — Notification sound picker, red badge, UI polish

--- build.sh
+++ build.sh
@@ -72,9 +72,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" << 'PLIST'
     <key>CFBundleIdentifier</key>
     <string>blog.insecurity.docket</string>
     <key>CFBundleVersion</key>
-    <string>1.0.0</string>
+    <string>1.1.0</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.0</string>
+    <string>1.1.0</string>
     <key>CFBundleExecutable</key>
     <string>Docket</string>
     <key>CFBundleIconFile</key>
