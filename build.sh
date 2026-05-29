136fe3eb957872d4625ccd01893d7ea10ec70d0a
santoru <alessiosantoru@gmail.com>
2026-05-29 16:02:58 +0200

    v1.5.2 — Natural language dates in edit view

--- build.sh
+++ build.sh
@@ -77,9 +77,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" << 'PLIST'
     <key>CFBundleIdentifier</key>
     <string>blog.insecurity.docket</string>
     <key>CFBundleVersion</key>
-    <string>1.5.1</string>
+    <string>1.5.2</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.5.1</string>
+    <string>1.5.2</string>
     <key>CFBundleExecutable</key>
     <string>Docket</string>
     <key>CFBundleIconFile</key>
